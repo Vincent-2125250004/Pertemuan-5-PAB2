07bd479990e69c9f8e4eb15c1d5dd860493e5e15
Vincent Pin <vincent.cent@mhs.mdp.ac.id>
2023-03-08 09:30:19 +0700

    Update Pertemuan 6
    Insert Values di Database

--- app/src/main/java/com/if4a/footballplayer/MyDatabaseHelper.java
+++ app/src/main/java/com/if4a/footballplayer/MyDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.if4a.footballplayer;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -34,9 +35,21 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(query);
 
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(query);
+        onCreate(db);
+    }
 
+    public long tambahPlayer (String nama, String nomor, String klub){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(FIELD_NAMA, nama);
+        cv.put(FIELD_NOMOR, nomor);
+        cv.put(FIELD_KLUB, klub);
+        long eksekusi = db.insert(TABLE_NAME, null, cv);
+        return eksekusi;
     }
 }
